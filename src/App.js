adc40b0b6c3a9bcbd11b642f56d65fea844c4510
Jonathan <jschack94@gmail.com>
2020-11-15 17:19:19 -0600

    Welcome Page created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  componentDidMount() {
+    this.floatingCandles();
+  }
+  
+  floatingCandles = () => {
+    let leftCandle = document.getElementById('left-candle');
+    let rightCandle = document.getElementById('right-candle');
+
+    setInterval(function() {
+      leftCandle.classList.toggle('candle-height');
+    }, 1300);
+
+    setInterval(function() {
+      rightCandle.classList.toggle('candle-height');
+    }, 1200);
+  };
+
+  render() {
+  
+      return (
+
+        <div className="wrapper">
+        <div id="left-candle" className="candle left">
+          <div className="flame" />
+          <div className="wax" />
+        </div>
+       <Welcome/>
+        <div id="right-candle" className="candle right">
+          <div className="flame" />
+          <div className="wax" />
+        </div>
+      </div>
+    );
+          
 }
-
+      
+}
 export default App;
+
+      
+
+
+
---
+++ src/Welcome.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Welcome.css';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div id="welcome-screen">
+        <div className="game-wrapper">
+          <h1>Welcome to the Harry Potter Quiz</h1>
+          <p>Are you ready to prove yourself a true fan?</p>
+          <button className="button" onClick={this.props.beginQuiz}>
+            I'm Ready
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
